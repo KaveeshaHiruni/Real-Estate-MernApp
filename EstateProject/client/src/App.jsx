07063a70e30634920419de77a1a77e742c11d14a
Kaveesha Hiruni <89571862+KaveeshaHiruni@users.noreply.github.com>
2024-02-09 15:26:07 +0530

    components added
    components added

--- EstateProject/client/src/App.jsx
+++ EstateProject/client/src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes, BrowserRouter} from "react-router-dom"
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Signin from './components/Signin';
+import Signout from './components/Signout';
+import About from './components/About';
+import Header from './pages/Header';
+
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}> </Route>
+          <Route path='/profile' element={<Profile/>}> </Route>
+          <Route path='/signin' element={<Signin/>}> </Route>
+          <Route path='/signout' element={<Signout/>}> </Route>
+          <Route path='/about' element={<About/>}> </Route>
+        </Routes>  
+      </BrowserRouter>
   )
 }
